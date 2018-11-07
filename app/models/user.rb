8c3aa25000d4affb077b0993d06dea494257e967
futamidaiki <nimi0370@gmail.com>
2018-11-08 00:30:25 +0900

    add enum to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: {admin:1,member: 2}
+  enum role: {admin:1, member: 2}
 end
