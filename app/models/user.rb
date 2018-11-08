7197cf6b698d0435ed3385ca637835d64876d488
futamidaiki <nimi0370@gmail.com>
2018-11-08 13:38:45 +0900

    Rspecを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #
+
+  validates :email, presence: true
+  validates :password, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: {admin:1, member: 2}
