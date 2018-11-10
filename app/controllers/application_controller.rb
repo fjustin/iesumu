daa8609144431fe99874a6fd149358a873c937aa
futamidaiki <nimi0370@gmail.com>
2018-11-10 16:42:55 +0900

    userモデルにavatarを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
-      devise_parameter_sanitizer.permit(:sing_uo, keys: [:avatar])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :avatar])
     end
 end
