6c7de9cd9fc3f4af5c553c3d4c12f2fed2df4019
futamidaiki <nimi0370@gmail.com>
2018-11-11 17:08:16 +0900

    rspecでUser登録のvalidationテストが通る

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
   describe "バリデーションの挙動をテスト" do
     it "emailとpasswordの両方が設定されていればOK" do
-      @user = User.new(email: "test@user",password: "000000")
+      @user = User.new(email: "test@user",password: "000000", username: "TEST MAN")
       expect(@user.valid?).to eq(true)
     end
 
     it "emailが空だとNG" do
-      @user = User.new(email: "",password: "000000")
+      @user = User.new(email: "",password: "000000", username: "TEST MAN")
       expect(@user.valid?).to eq(false)
     end
 
     it "passwordが空だとNG" do
-      @user = User.new(email: "test@user", password: "")
+      @user = User.new(email: "test@user", password: "", username: "TEST MAN")
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "usernameが空だとvalidationが働く" do
+      @user = User.new(email: "test@user", password: "999999999", username: "")
       expect(@user.valid?).to eq(false)
     end
   end
