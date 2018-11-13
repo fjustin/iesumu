60f341312b011a1f5ecf48bcc8d22237aa9078b6
futamidaiki <nimi0370@gmail.com>
2018-11-13 13:36:03 +0900

    同じ投稿が保存される

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe House, type: :model do
     before do
     end
 
+    # userとhouseは度々利用するためローカル変数化
     let(:user) {User.create(email: "test@test", password: "password",username: "TEST MAN")}
     let(:house) {House.new(name: "text",content: "content", user_id: user.id)}
 
 
     it "nameとcontentの両方があるとOK" do
-      house = House.create(name: "text", content: "content",user_id: user.id)
       expect(house).to be_valid
     end
 
@@ -40,6 +40,14 @@ RSpec.describe House, type: :model do
       it "タイトル文字制限を超えて投稿" do
         house.name = "a" * 100
         expect(house).to be_invalid
+      end
+    end
+
+    context "投稿が被った場合の挙動" do
+      it "同じ投稿があってもちゃんと保存される" do
+        house.save!
+        dup_house = house.dup
+        expect(dup_house.save).to be_truthy
       end
     end
   end
