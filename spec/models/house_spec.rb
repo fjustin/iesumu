7e8815b3ce87554b36e07f2d0e877001840c0765
futamidaiki <nimi0370@gmail.com>
2018-11-13 13:27:18 +0900

    文字制限のrspec

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe House, type: :model do
     end
 
     let(:user) {User.create(email: "test@test", password: "password",username: "TEST MAN")}
-    let(:house) {House.create(name: "text",content: "content", user_id: user.id)}
+    let(:house) {House.new(name: "text",content: "content", user_id: user.id)}
 
 
     it "nameとcontentの両方があるとOK" do
@@ -24,6 +24,23 @@ RSpec.describe House, type: :model do
     it "nameが空だとNG" do
       house.name = ""
       expect(house.save).to be_falsey
+    end
+
+    it "contentが空だとNG" do
+      house.content = ""
+      expect(house.save).to be_falsey
+    end
+
+    context "シェアハウス投稿のタイトルが50文字制限" do
+      it "タイトル文字制限内で無事投稿される" do
+        house.name = "a" * 50
+        expect(house).to be_valid
+      end
+
+      it "タイトル文字制限を超えて投稿" do
+        house.name = "a" * 100
+        expect(house).to be_invalid
+      end
     end
   end
 end
