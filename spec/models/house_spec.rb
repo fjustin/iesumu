9b66d8e67ed6a6cd7b7cd5f62fa4fcaee22c2b16
futamidaiki <nimi0370@gmail.com>
2018-11-17 16:30:51 +0900

    enumerizeのrspecテストを追加

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 RSpec.describe House, type: :model do
 
   describe "シェアハウス投稿のvalidationを確認" do
+
+    # enumerizeを検証
+    extend Enumerize
+    enumerize :status,in: [ :empty, :full ],default: :empty, scope: true
+
     before do
     end
 
     # userとhouseは度々利用するためローカル変数化
-    let(:user) {User.create(email: "test@test", password: "password",username: "TEST MAN")}
-    let(:house) {House.new(name: "text",content: "content", user_id: user.id)}
+    let(:user) { User.create(email: "test@gmail.com", password: "password",username: "TEST MAN") }
+    let(:house) { House.new(name: "text",content: "content", user_id: user.id,status: :empty) }
 
 
     it "nameとcontentの両方があるとOK" do
@@ -53,6 +58,16 @@ RSpec.describe House, type: :model do
         house.save!
         dup_house = house.dup
         expect(dup_house.save).to be_truthy
+      end
+    end
+
+    context "statusを確認" do
+      it "emptyだった時true" do
+        expect(house.status).to eq'empty'
+      end
+
+      it "fullだった時false" do
+        expect(house.status).not_to eq("full")
       end
     end
   end
