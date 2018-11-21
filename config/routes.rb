119883dab42232f951c4efdd94b2fdbf98c8177b
futamidaiki <nimi0370@gmail.com>
2018-11-21 19:44:00 +0900

    DM機能が完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :houses
   resources :users, :only => [:index,:show]
   resources :messages, :only => [:create]
   resources :rooms, :only => [:create, :show, :index]
-  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
