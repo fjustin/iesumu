84b67395afb5bf39623f920435b1ecfcb1dc2ca3
futamidaiki <nimi0370@gmail.com>
2018-11-22 16:21:08 +0900

    シェアハウス詳細のvalidation

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,7 +5,7 @@ class House < ApplicationRecord
   # 1を募集中、2を居住中、デフォルトを1とする。スコープとして指定するので募集ページと居住ページで分ける予定
   enumerize :status,in: [ :empty, :full ],default: :empty, scope: true
 
-  validates :name,:content, presence: true
+  validates :name,:content,:price,:member,:address,:station, presence: true
   validates :name, length: { maximum: 50 }
 
   belongs_to :user
