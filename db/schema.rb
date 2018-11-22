e685e867a0df8a9ca33694b571d46d184b46cbca
futamidaiki <nimi0370@gmail.com>
2018-11-22 15:53:45 +0900

    シェアハウスの詳細を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_080200) do
+ActiveRecord::Schema.define(version: 2018_11_22_065255) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -76,6 +76,10 @@ ActiveRecord::Schema.define(version: 2018_11_21_080200) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "status"
+    t.integer "member"
+    t.integer "price"
+    t.string "address"
+    t.string "station"
   end
 
   create_table "messages", force: :cascade do |t|
