d3fdf94271fadc9d9357058e9eafe5ee268b803b
futamidaiki <nimi0370@gmail.com>
2018-11-24 16:13:37 +0900

    houseからstationを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_141018) do
+ActiveRecord::Schema.define(version: 2018_11_24_071255) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 2018_11_23_141018) do
     t.integer "member"
     t.integer "price"
     t.string "address"
-    t.string "station"
   end
 
   create_table "messages", force: :cascade do |t|
