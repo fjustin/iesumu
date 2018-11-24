54ad3824fb6857561b57dcab7af3b24cd7080402
futamidaiki <nimi0370@gmail.com>
2018-11-24 16:22:24 +0900

    migrationを作成してmigrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :stations
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :houses
   resources :users, :only => [:index,:show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_071255) do
+ActiveRecord::Schema.define(version: 2018_11_24_071728) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_11_24_071255) do
     t.integer "member"
     t.integer "price"
     t.string "address"
+    t.integer "station_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -97,6 +98,12 @@ ActiveRecord::Schema.define(version: 2018_11_24_071255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "house_id"
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
